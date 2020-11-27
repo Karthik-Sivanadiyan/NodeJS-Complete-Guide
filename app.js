06877b17fec6afb8714455b067873a1f7ef620b5
karthik-sivanadiyan <ksivanad@usc.edu>
2020-11-26 18:10:39 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+// Anonymous function 
+const server = http.createServer((req, res) => {
+  // Parse URL
+  const url = req.url;
+  // Triple equals checks type and value
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+  res.write('</html>')
+  return res.end()
+})
+
+// Start the server
+server.listen(3000)
